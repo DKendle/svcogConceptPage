d4a072e792a98be933f474fe89151491097c6556
DKendle <dina.kendle@gmail.com>
2022-07-28 13:42:43 -0400

    adds test data and ignore file

---
+++ index.js
@@ -0,0 +1,7 @@
+const comments = document.getElementsByClassName("comments")
+
+function getComments(){
+    //Access all comments and grab them
+    //list them in order by date
+    //display them
+}
